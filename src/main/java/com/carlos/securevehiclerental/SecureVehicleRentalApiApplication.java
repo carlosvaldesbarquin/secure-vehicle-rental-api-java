3cc4ca47d9a8c50c2b37d05458d6c1f385fda48a
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2026-06-16 17:13:16 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/carlos/securevehiclerental/SecureVehicleRentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.securevehiclerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVehicleRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureVehicleRentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/securevehiclerental/SecureVehicleRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.securevehiclerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVehicleRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
